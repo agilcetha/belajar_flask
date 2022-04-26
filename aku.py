5df2b2fc1bbf64e8d34331567867a567adb4fdbb
rahmadae <rahmad.h22@gmail.com>
2022-04-26 22:20:13 +0700

    nambah file

--- aku.py
+++ aku.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask (__name__)
 
-@app.route("/helo")
+@app.route("/")
 def hello():
     return "halaman opo iki"
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug=True) # penulisan debug=True tidak usah spasi
