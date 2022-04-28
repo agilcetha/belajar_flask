d209c89e5f069ddca01f7709c538f0bc4137cc41
rahmadae <rahmad.h22@gmail.com>
2022-04-28 09:31:25 +0700

    nambah file

--- aku.py
+++ aku.py
@@ -4,7 +4,7 @@ app = Flask (__name__)
 
 @app.route("/")
 def hello():
-    return "<h1>halaman opo iki</h1>"
+    return "<h1>Hello RAHMAD</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True) # penulisan debug=True tidak usah spasi
