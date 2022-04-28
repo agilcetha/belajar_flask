6cd19b01bbb514fbb05be0bb1d2d2ddd6f976b79
rahmadae <rahmad.h22@gmail.com>
2022-04-28 09:46:24 +0700

    nambah file

--- aku.py
+++ aku.py
@@ -6,5 +6,5 @@ app = Flask (__name__)
 def hello():
     return "<h1>Hello RAHMAD</h1>"
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True) # penulisan debug=True tidak usah spasi
