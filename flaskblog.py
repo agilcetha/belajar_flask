4b279e17fb8bfeb8c334496508bc4593880785d7
rahmadae <rahmad.h22@gmail.com>
2022-05-15 20:58:01 +0700

    ganti file

--- flaskblog.py
+++ flaskblog.py
@@ -4,11 +4,11 @@ app = Flask (__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Home RAHMAD</h1>"
+    return "<h1>Home AHSAN</h1>"
 
 @app.route("/about")
 def about():
-    return "<h1>About Rahmad</h1>"
+    return "<h1>About  AHSAN</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True) # penulisan debug=True tidak usah spasi
