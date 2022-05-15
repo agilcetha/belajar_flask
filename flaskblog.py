7061fbaea6679687c447d3ddac4fee67262f376b
agilcetha <agilcetha@gmail.com>
2022-05-15 19:42:00 +0700

    menambah file baru

---
+++ flaskblog.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hallo, cuy!"
+
+@app.route('/about')
+def hello():
+    return "about, cuy!"
+
+# if __name__ == "__main__":
+    # app.run(debug=True)
