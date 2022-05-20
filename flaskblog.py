74afe10ea6cb4cc6abcad151b2e9f4fb7f29be85
rahmadae <rahmad.h22@gmail.com>
2022-05-20 19:47:00 +0700

    nambah beberapa file

--- flaskblog.py
+++ flaskblog.py
@@ -1,14 +1,28 @@
-from flask import Flask
-
+from flask import Flask, render_template
 app = Flask (__name__)
+
+posts = [
+    {
+        'author': 'Rahmad Hidayat',
+        'title' : 'Blog Post 1',
+        'content': 'Post Konten Pertama',
+        'date_posted': 'May 20, 2022'
+    },
+    {
+        'author': 'Ahmad Ahsan',
+        'title' : 'Blog Post 2',
+        'content': 'Post Konten Kedua',
+        'date_posted': 'May 21, 2022'
+    }
+]
 
 @app.route("/")
 def home():
-    return "<h1>Home AHSAN</h1>"
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-    return "<h1>About  AHSAN</h1>"
+    return render_template('about.html')
 
 if __name__ == "__main__":
     app.run(debug=True) # penulisan debug=True tidak usah spasi
