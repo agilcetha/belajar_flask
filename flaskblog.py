50b8b9560f0a577e010313d01158c324e7901a57
PetengDedet <pueteng.dedet@gmail.com>
2022-05-28 12:52:43 +0700

    OK video 2

--- flaskblog.py
+++ flaskblog.py
@@ -1,15 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+posts = [
+    {
+        "author": "Corey Schafer",
+        "title": "Blog Post 1",
+        "content": "Content 1",
+        "date_posted": "May, 28 2022"
+    },
+    {
+        "author": "Corey Schafer",
+        "title": "Blog Post 2",
+        "content": "Content 2",
+        "date_posted": "May, 27 2022"   
+    }
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Hello World!</h1>"
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html', title="About page")
 
 
 if __name__ == '__main__':
