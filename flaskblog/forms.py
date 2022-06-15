da9a8900a94aeb6a8c8353e7ef9720f25903d7e4
rahmadae <rahmad.h22@gmail.com>
2022-06-15 22:07:11 +0700

    finish

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -21,4 +21,3 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
-    
